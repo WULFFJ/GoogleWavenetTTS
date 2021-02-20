6fdb29cb9e98f4eab070a32fa13a8b2148750b19
James <78693214+WULFFJ@users.noreply.github.com>
2021-02-19 16:59:49 -0800

    Update Texter.py

--- Texter.py
+++ Texter.py
@@ -2,7 +2,7 @@ import os
 chuck =0
 while chuck<100:
     talk=input("What should I say:")
-    os.environ['Google_Application_Credentials'] = 'XXXWHERE YOU WOULD LIKE YOUR MP3 TO BE SAVEDXXX'
+    os.environ['Google_Application_Credentials'] = 'XXXWHERE YOU PLACED YOUR JSON FILE WITH CREDENTIALSXXX'
     tts_client = texttospeech_v1.TextToSpeechClient()
     params = texttospeech_v1.VoiceSelectionParams(language_code='en-IN', name="en-IN-Wavenet-D")
     audio = texttospeech_v1.AudioConfig(audio_encoding = texttospeech_v1.AudioEncoding.MP3)
@@ -11,5 +11,5 @@ while chuck<100:
     f = open('says.mp3', 'wb')
     f.write(response.audio_content)
     f.close()
-    os.system("start says.mp3")
+    os.system("WHATEVERYOUWANT.mp3")
     chuck=chuck+1
